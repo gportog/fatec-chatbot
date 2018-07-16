2aa1737d2f20123f25222281eb4ca6a9334e947c
Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>
2018-07-15 21:30:45 -0300

    Refactor the Front-end to use 'routes' :art:
    On the previous structure the code wasn't using well the React.js.
    All the html was on the index.html.
    
    Now the index.html just have some main scripts that we're going to
    use on our Front-end - such as Bootstrap.css - and its just renders
    the 'app' id.
    
    The app id calls the App.js that divides the containers on routes.
    On the first scenario we're going to have three routes: /, /login and
    /chatbot that renders the containers <Login /> and <ChatbotDash />
    respectively.
    
    The ChatbotDash calls the Chatbot screen that uses the Chatbot service.
    
    Signed-off-by: Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>

---
+++ client/src/containers/ChatbotDash.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+import Chatbot from '../components/Chatbot/Chatbot';
+import user from './img/anonymous_user.png';
+import './ChatbotDash.css';
+
+class ChatbotDash extends Component {
+    constructor() {
+        super();
+        this.state = {
+            showUsrMng: false
+        }
+    }
+
+    UserManagement() {
+        this.setState({
+            showUsrMng: !this.state.showUsrMng
+        });
+    }
+
+    componentDidMount() {
+        let btnMenu = document.getElementById('btn-menu');
+        let nav = document.getElementById('nav');
+        let $doc = $('html, body');
+        // show menu items
+        btnMenu.addEventListener('click', function () {
+            nav.classList.toggle('show');
+        })
+        // scroll effect - click on menu items
+        $('a').click(function () {
+            $doc.animate({
+                scrollTop: $($.attr(this, 'href')).offset().top
+            }, 1000);
+            return false;
+        });
+    }
+
+    render() {
+        let usrMng = (
+            <div>
+                <div id="header-UsrMng-box">
+                    <p className="header-UsrInfo">
+                        <br></br>Usuário<br></br>
+                        <br></br>
+                        <p className="header-Logout" title="log out"><b><span className="glyphicon glyphicon-log-out"></span> Log Out</b></p>
+                    </p>
+                </div>
+            </div>);
+
+        return (
+            <main>
+                <div className="header">
+                    <div className="btn_menu">
+                        <span className="icon-menu" id="btn-menu" title="Configurações do usuário"></span>
+                        <nav className="nav" id="nav">
+                            <ul className="menu">
+                                <li className="menu__item">
+                                    <a className="menu__link" id="link1" href="#chatbot">Chat</a>
+                                </li>
+                                <li className="menu__item">
+                                    <a className="menu__link" id="link2" href="#team">Time</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <img src={user} className="header-user" alt="Foto do usuário" title="Configurações do usuário"
+                        onClick={this.UserManagement.bind(this)} />
+                    {this.state.showUsrMng ? usrMng : null}
+                </div>
+
+                <div className="container" id="chatbot">
+                    <div id="root"><Chatbot /></div>
+                </div>
+
+                <div className="container" id="team">
+                    <div className="row">
+                        <h3 className="title">Time</h3>
+
+                        <div className="col-xs-6 col-sm-6 col-md-3">
+                            <div className="cards">
+                                <h4 className="name">Gustavo Guedes</h4>
+                                <img src="img/team/gportog.png" className="pics" />
+                                <div className="role">Desenvolvedor</div>
+                            </div>
+                        </div>
+                        <div className="col-xs-6 col-sm-6 col-md-3">
+                            <div className="cards">
+                                <h4 className="name">Isabella Leite</h4>
+                                <img src="img/team/isa.jpg" className="pics" />
+                                <div className="role">Desenvolvedora</div>
+                            </div>
+                        </div>
+                        <div className="col-xs-6 col-sm-6 col-md-3">
+                            <div className="cards">
+                                <h4 className="name">Leticia Costa</h4>
+                                <img src="img/team/le.jpg" className="pics" />
+                                <div className="role">Desenvolvedora</div>
+                            </div>
+                        </div>
+                        <div className="col-xs-6 col-sm-6 col-md-2">
+                            <div className="cards">
+                                <h4 className="name">Humberto Zanetti</h4>
+                                <img src="img/team/humberto.jpg" className="pics" />
+                                <div className="role">Orientador</div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <p id="terms-of-use">Termos de uso</p>
+            </main>
+        );
+    }
+}
+
+export default ChatbotDash;
