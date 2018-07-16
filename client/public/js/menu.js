5cfa065ee5dc937077320493859d5a1e7b29b603
Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>
2018-07-15 21:30:45 -0300

    Refactor the Front-end to use 'routes' :art:
    On the previous structure the code wasn't using well the React.js.
    All the html was on the index.html.
    
    Now the index.html just have some main scripts that we're going to
    use on our Front-end - such as Bootstrap.css - and its just renders
    the 'app' id.
    
    The app id calls the App.js that divides the containers on routes.
    On the first scenario we're going to have three routes: /, /login and
    /chatbot that renders the containers <Login /> and <ChatbotDash />
    respectively.
    
    The ChatbotDash calls the Chatbot screen that uses the Chatbot service.
    
    Signed-off-by: Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>

--- client/public/js/menu.js
+++
@@ -1,6 +0,0 @@
-var btnMenu = document.getElementById('btn-menu');
-var nav = document.getElementById('nav');
-
-btnMenu.addEventListener('click', function(){
-    nav.classList.toggle('mostrar');
-})
--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,104 +1,24 @@
 import React, { Component } from 'react';
-import Conversation from '../services/conversation';
-import ProjectTeam from '../components/ProjectTeam';
-import './App.css';
+import ChatbotDash from './ChatbotDash';
+import {
+  HashRouter,
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from "react-router-dom";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      conversationHistory: []
-    };
-  }
-
-  componentDidMount() {
-    this.sendMessage('')
-  }
-
   render() {
     return (
-     <div className="app">     
-      <header>Fatec Chatbot</header>
-      <section ref="main">
-       { this.state.conversationHistory.map((h,i) => 
-         this.renderExchange(h, i)) }
-      </section>
-      <footer>
-      { this.renderInputView() }
-      </footer>
-     </div>
+      <HashRouter>
+        <Switch>
+          <Route path="/chatbot" component={ChatbotDash} />
+          <Route path="/" component={ChatbotDash} />
+        </Switch>
+      </HashRouter>
     );
-  }
-
-  renderExchange(exchange, key) {
-    return !!exchange.output.cardType
-      ? this.renderCard(exchange, key)
-      : this.renderText(exchange, key);
-  }
-
-  renderCard(exchange, key) {
-    switch(exchange.output.cardType) {
-      case 'project_team':  
-       return (
-        <div key={key} className="exchange">
-          { exchange.input.text ? <div className="user-msg">{exchange.input.text}</div> : null }
-          <div className="watson-msg"><ProjectTeam/></div>
-        </div>);   
-      default:
-        this.renderText(exchange,key);
-    }
-  }
-
-  renderText(exchange, key) {
-    return (
-      <div key={key} className="exchange">
-        { exchange.input.text ? <div className="user-msg">{exchange.input.text}</div> : null }
-        { exchange.output.text.map((t, i) => <div key={i} className="watson-msg">{t}</div>) }
-      </div>);
-  }
-
-  renderInputView() {
-    return <input type="text" autoComplete="off" placeholder='Escreva aqui...'
-                  onKeyUp={e => this.onInputKeyUp(e)}/>;
-  }
- 
-
-  onInputKeyUp(e) {
-    switch (e.which) {
-      case 0x0d:
-        this.sendMessage(e.target.value);
-        e.target.value = '';      
-        break;
-      default:
-        break;
-    }
-  }
-
-  sendMessage(text) {
-    Conversation.message({
-      text
-    }).then(r => {
-      this.state.conversationHistory.push(r);
-      this.setState({
-        conversationHistory: this.state.conversationHistory
-      })
-    });
-  }
-
-  componentDidUpdate() {
-    const scrollTo = (element, to, duration) => {
-      if (duration <= 0) return;
-      const difference = to - element.scrollTop;
-      const perTick = difference / duration * 10;
-
-      setTimeout(function () {
-        element.scrollTop += perTick;
-        if (element.scrollTop === to) return;
-        scrollTo(element, to, duration - 10);
-      }, 10);
-    };
-    const node = this.refs.main;
-    scrollTo(node, node.scrollHeight, 300);
   }
 }
 
