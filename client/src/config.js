92f67e3d14be6e553308534f2d8ef0de152a853d
Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>
2018-07-28 13:59:43 -0300

    Add Feedback route to the Back-End :heavy_plus_sign:
    This route can be reached by a POST request to /api/v1/feedback/
    It makes a validation of the json passed on the body. If the evaluation or
    comment keys aren't there or with wrong type the route sends a 400
    message to the user, otherwise it goes to its pipeline - that is
    composed by the EmailClient and DBClient service.
    
    Signed-off-by: Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const apiHost = '';
-const apiRoot = `${apiHost}/watsonconversation/api/v1`
+const apiRoot = `${apiHost}/api/v1`
 
 export default {
   apiRoot
---
+++ server/api/controllers/feedback/feedback.js
@@ -0,0 +1,46 @@
+const DBClient = require('../../services/DBClient');
+const EmailClient = require('../../services/EmailClient');
+
+module.exports = (req, res, next) => {
+    if (!req.body.evaluation || typeof req.body.evaluation !== 'string') {
+        let err = new Error(`Received ` + typeof req.body.evaluation + ` for field 'evaluation'. ` +
+            `Expected a string.`);
+        err.status = 400;
+        return next(err)
+    }
+    if (typeof req.body.comment !== 'string' && typeof req.body.comment !== 'undefined') {
+        let err = new Error(`Received ` + typeof req.body.comment + ` for field 'comment'. ` +
+            `Expected a string.`);
+        err.status = 400;
+        return next(err)
+    }
+    const dbInstance = new DBClient('chatbot_feedback');
+    const emailClient = new EmailClient();
+    const date = new Date();
+    let feedbackEntry = {
+        user: req.session ? req.session.user.name : 'username',
+        contact: req.session ? req.session.user.contact : 'contato',
+        topic: req.body.topic || '',
+        history: req.body.history || [],
+        date: `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`,
+        comment: req.body.comment,
+        evaluation: req.body.evaluation
+    }
+    let emailMessage = "<body>" +
+        "<div>" +
+        "<h3>FATEC-JD Chatbot</h3>" +
+        `<p>Você recebeu um feedback ${feedbackEntry.evaluation} do/a ${feedbackEntry.contact}</p>` +
+        `<p><b>Mensagem: </b>${feedbackEntry.comment}</p>` +
+        `<p>Para mais informações, acesse o <a href="https://fatec-chabot-dev.mybluemix.net/#/" target="blank">Admin Dashboard</a>.</p>` +
+        "</div>" +
+        "</body>";
+    emailClient.send(`Você recebeu um feedback do/a ${feedbackEntry.user}`, emailMessage)
+        .then(() => dbInstance.insert(feedbackEntry))
+        .then((resp) => {
+            return res.status(200).json(resp);
+        })
+        .catch((error) => {
+            let err = new Error('Internal server error');
+            return next(err)
+        });
+}
